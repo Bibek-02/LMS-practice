69c2207d24334f5cd16ba9f37e241ca866d0c954
Bibek-02 <bibekadhikari081@gmail.com>
2025-10-31 23:22:59 +0100

    server setup pratice "day 2"

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mongoose from 'mongoose';  
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({ message: 'Server running successfully ✅' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
+
